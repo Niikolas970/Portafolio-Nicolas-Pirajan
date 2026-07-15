f2694c0e79c8902807452934041d222f5fca6a59
NicolasPirajan <nicolasalejandro2001@gmail.com>
2026-07-14 21:19:20 -0500

    feat: completar la estructura base del Navbar

--- react/src/components/layouts/Navbar/Navbar.jsx
+++ react/src/components/layouts/Navbar/Navbar.jsx
@@ -6,36 +6,54 @@ import "./Navbar.scss";
 function Navbar() {
     return (
         <nav className="navbar">
-            <div className="navbar__logo">
-                <a href="#hero">
+            <div className="navbar__container">
+
+                {/* Logo */}
+                <a className="navbar__brand" href="#hero">
                     &lt; Nicolás /&gt;
                 </a>
-            </div>
-            <ul className="navbar__menu">
-                {navigation.map((item) => (
-                    <li key={item.id}>
-                        <a href={item.href}>
-                            {item.label}
-                        </a>
-                    </li>
-                ))}
-            </ul>
-            <div className="navbar__actions">
-                {socialLinks.map((item) => {
-                    const Icon = item.icon;
-                    return (
-                        <a
+
+                {/* Menú */}
+                <ul className="navbar__menu">
+                    {navigation.map((item) => (
+                        <li
+                            className="navbar__item"
                             key={item.id}
-                            href={item.href}
-                            target="_blank"
-                            rel="noopener noreferrer"
-                            aria-label={item.label}
                         >
-                            <Icon size={20} />
-                        </a>
-                    );
-                })}
-                <ThemeToggle/>
+                            <a
+                                className="navbar__link"
+                                href={item.href}
+                            >
+                                {item.label}
+                            </a>
+                        </li>
+                    ))}
+                </ul>
+
+                {/* Acciones */}
+                <div className="navbar__actions">
+
+                    {socialLinks.map((item) => {
+                        const Icon = item.icon;
+
+                        return (
+                            <a
+                                className="navbar__social"
+                                key={item.id}
+                                href={item.href}
+                                target="_blank"
+                                rel="noopener noreferrer"
+                                aria-label={item.label}
+                            >
+                                <Icon size={20} />
+                            </a>
+                        );
+                    })}
+
+                    <ThemeToggle />
+
+                </div>
+
             </div>
         </nav>
     );
